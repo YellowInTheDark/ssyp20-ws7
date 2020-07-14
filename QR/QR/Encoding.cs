c372c9d620ea450d1ee777c4850fd5b52dbc36a2
MrTheFall <mrthefall@yandex.ru>
2020-07-14 17:12:39 +0700

    Moved dictionary to another file

--- QR/QR/Encoding.cs
+++ QR/QR/Encoding.cs
@@ -101,14 +101,14 @@ namespace QR
             string encodedLine = string.Empty;
             for (int i = 0; i < input.Length - 1; i += 2)
             {
-                var fElement = AlphanumericDictionary(input[i]);
-                var sElement = AlphanumericDictionary(input[i + 1]);
+                var fElement = Dicts.AlphanumericDictionary(input[i]);
+                var sElement = Dicts.AlphanumericDictionary(input[i + 1]);
                 var sum = fElement * 45 + sElement;
                 encodedLine += $"{Convert.ToString(sum, 2).PadLeft(11, '0')} ";
             }
             if (input.Length % 2 == 1)
             {
-                int tmp = AlphanumericDictionary(input[input.Length - 1]);
+                int tmp = Dicts.AlphanumericDictionary(input[input.Length - 1]);
                 encodedLine += $"{Convert.ToString(tmp, 2).PadLeft(6, '0')} ";
             }
 
@@ -204,61 +204,6 @@ namespace QR
             encodedLine = DivideLine(encodedLine);
             encodedLine = AddCodewords(encodedLine, version, correctionLevel);
             return encodedLine;
-        }
-
-        static int AlphanumericDictionary(char keyValue)
-        {
-            Dictionary<char, int> AD = new Dictionary<char, int>
-            {
-
-                {'0', 0 },
-                {'1', 1 },
-                {'2', 2 },
-                {'3', 3 },
-                {'4', 4 },
-                {'5', 5 },
-                {'6', 6 },
-                {'7', 7 },
-                {'8', 8 },
-                {'9', 9 },
-                {'A', 10 },
-                {'B', 11 },
-                {'C', 12 },
-                {'D', 13 },
-                {'E', 14 },
-                {'F', 15 },
-                {'G', 16 },
-                {'H', 17 },
-                {'I', 18 },
-                {'J', 19 },
-                {'K', 20 },
-                {'L', 21 },
-                {'M', 22 },
-                {'N', 23 },
-                {'O', 24 },
-                {'P', 25 },
-                {'Q', 26 },
-                {'R', 27 },
-                {'S', 28 },
-                {'T', 29 },
-                {'U', 30 },
-                {'V', 31 },
-                {'W', 32 },
-                {'X', 33 },
-                {'Y', 34 },
-                {'Z', 35 },
-                {' ', 36 },
-                {'$', 37 },
-                {'%', 38 },
-                {'*', 39 },
-                {'+', 40 },
-                {'-', 41 },
-                {'.', 42 },
-                {'/', 43 },
-                {':', 44 },
-            };
-            return AD[keyValue];
-
         }
 
         public static int GetVersion(byte[] bytes, int correctionLevel)
