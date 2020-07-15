1cc51225ba9d9b58540932023445ab198f1fbc43
SolomennikovKolya <52555203+SolomennikovKolya@users.noreply.github.com>
2020-07-15 10:38:34 +0300

    //

--- QR code/QR code/Program.cs
+++ QR code/QR code/Program.cs
@@ -15,7 +15,8 @@ namespace QR_code
             bool containsByte = false;
             foreach (char i in line)
             {
-                if (Char.IsNumber(i)) containsNumeric = true;
+                int j = (int)i;
+                if (48 <= j && j <= 57) containsNumeric = true;
                 else if (Char.IsLetter(i)) containsAlphanumeric = true;
                 else if (8140 >= (int)i && (int)i >= 40956 || 57408 >= (int)i && (int)i <= 60351) containsKanji = true;
             }
@@ -26,6 +27,12 @@ namespace QR_code
         }
         static void Main()
         {
+            // Console.WriteLine($"{(int)' '} {(int)'$'} {(int)'%'} {(int)'*'} {(int)'+'} {(int)'-'} {(int)'.'} {(int)'/'} {(int)':'}");
+            /*
+            0	1	2	3	4	5	6	7	8	9	// [48; 57]
+            A	B	C	D	E   F	G	H	I	J	K	L	M	N	O	P	Q	R	S	T   U	V	W	X	Y	Z	// [65; 90]
+            Пробел	$	%	*	+	-	.	/	: // 32 36 37 42 43 45 46 47 58
+            */
             string line = "";
             try
             {
