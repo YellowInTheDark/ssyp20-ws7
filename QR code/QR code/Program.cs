657e12a4fae69be8b3dc0d8bfc989f4f07c8d9d3
SolomennikovKolya <52555203+SolomennikovKolya@users.noreply.github.com>
2020-07-15 10:58:10 +0300

    fix ifs

--- QR code/QR code/Program.cs
+++ QR code/QR code/Program.cs
@@ -9,30 +9,25 @@ namespace QR_code
         static string BestMode(string line)
         {
             // numeric, alphanumeric, byte, kanji
-            bool containsNumeric = false;
-            bool containsAlphanumeric = false;
-            bool containsKanji = false;
-            bool containsByte = false;
+            bool numeric = false;
+            bool alphanumeric = false;
+            bool kanji = false;
+            bool other = false;
             foreach (char i in line)
             {
                 int j = (int)i;
-                if (48 <= j && j <= 57) containsNumeric = true;
-                else if (Char.IsLetter(i)) containsAlphanumeric = true;
-                else if (8140 >= (int)i && (int)i >= 40956 || 57408 >= (int)i && (int)i <= 60351) containsKanji = true;
+                if (48 <= j && j <= 57) numeric = true;
+                else if (65 <= j && j <= 90 || j == 32 || j == 36 || j == 37 || j == 42 || j == 43 || j == 43 || j == 45 || j == 46 || j == 47 || j == 58) alphanumeric = true;
+                else if (8140 >= (int)i && (int)i >= 40956 || 57408 >= (int)i && (int)i <= 60351) kanji = true;
+                else other = true;
             }
-            if (containsNumeric && !containsAlphanumeric && !containsKanji && !containsByte) return "numeric";
-            else if (containsAlphanumeric && !containsKanji && !containsByte) return "alphanumeric";
-            else if (containsKanji && !containsByte) return "kanji";
+            if (numeric && !alphanumeric && !kanji && !other) return "numeric";
+            else if (alphanumeric && !kanji && !other) return "alphanumeric";
+            else if (!numeric && !alphanumeric && kanji && !other) return "kanji";
             else return "byte";
         }
         static void Main()
         {
-            // Console.WriteLine($"{(int)' '} {(int)'$'} {(int)'%'} {(int)'*'} {(int)'+'} {(int)'-'} {(int)'.'} {(int)'/'} {(int)':'}");
-            /*
-            0	1	2	3	4	5	6	7	8	9	// [48; 57]
-            A	B	C	D	E   F	G	H	I	J	K	L	M	N	O	P	Q	R	S	T   U	V	W	X	Y	Z	// [65; 90]
-            Пробел	$	%	*	+	-	.	/	: // 32 36 37 42 43 45 46 47 58
-            */
             string line = "";
             try
             {
